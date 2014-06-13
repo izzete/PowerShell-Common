b863980a3216b2b38af2829279adc553aaa0ac3f
izzete <izzet.ergas@citrix.com>
2014-06-13 17:25:33 -0400

    Cleanup  Get-CachedADGroup

--- Get-ADGroups.ps1
+++ Get-ADGroups.ps1
@@ -188,9 +188,8 @@ function Get-ADGroups {
 }
 
 function Get-CachedADGroups {
-    Write-Verbose "Reading ADGroup info"
-    # Uncomment line below to query AD, otherwise it uses cached results
-    $global:Results = Import-Csv (ls $ResultsPath\ADGroup* | sort -Descending Name | select -First 1).FullName
+    Write-Verbose "Reading cached ADGroup info"
+    $global:Results = Import-Csv (ls $global:ResultsPath\ADGroup* | sort -Descending Name | select -First 1).FullName
     Write-Verbose "Found $($Results.Count) groups"
 }
 
