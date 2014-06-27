0fb9c06306b5b32fe81d883b51c510c083c6664b
izzete <izzet.ergas@citrix.com>
2014-06-27 19:48:22 -0400

    Break out init code

--- Get-ADGroups.ps1
+++ Get-ADGroups.ps1
@@ -1,43 +1,5 @@
 ﻿[CmdletBinding()]
-Param ()
-
-$VerbosePreference = 'SilentlyContinue'             # set to SilentlyContinue to disable, Continue to enable
-$DebugPreference   = 'SilentlyContinue'             # set to SilentlyContinue to disable, Continue to enable
-$WarningPreference = 'SilentlyContinue'             # set to SilentlyContinue to disable, Continue to enable
-
-# Determine location of work folders
-if ($psISE) {
-    # If running in debugger
-    $global:ScriptName = $psISE.CurrentFile.DisplayName   # -replace ".ps1", ""
-    $global:ScriptPath = Split-Path -Parent -Path $psISE.CurrentFile.FullPath
-}
-else {
-    # Else running in powershell console
-    $global:ScriptName = $MyInvocation.MyCommand.Name
-    $global:ScriptPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition 
-    #$global:ScriptPath  = $PSScriptRoot                         # PowerShell 3+
-}
-
-$global:LogPath     = $ScriptPath + "\Logs"
-$global:ResultsPath = $ScriptPath + "\Results" 
-$global:PicklePath  = $ScriptPath + "\Pickles"
-
-# Create work folders
-New-Item -ItemType Directory -Force -Path $LogPath | Out-Null
-New-Item -ItemType Directory -Force -Path $ResultsPath | Out-Null
-New-Item -ItemType Directory -Force -Path $PicklePath | Out-Null
-
-# Get host info for system the script is running on
-$reg = Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\" | select "NV Hostname", "NV Domain"
-$global:HostFQDN = ($reg.'NV Hostname' + "." + $reg.'NV Domain').ToLower()
-$global:Hostname = ($reg.'NV Hostname').ToUpper()
-
-# Setup log file
-$global:Timestamp = $(Get-Date -f yyyyMMdd_HHmmss)
-$global:LogFile   = $ScriptName -replace ".ps1", "_${Hostname}_${Timestamp}.log"
-
-
-#Start-Transcript "$LogFolder\$($Server)_$($Timestamp).log" -ErrorAction SilentlyContinue
+param ()
 
 function Get-RecipientType($Value)  {
 <#
