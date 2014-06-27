1da92f35b130e249a8ae66ae80fc47f821e44191
izzete <izzet.ergas@citrix.com>
2014-06-27 14:10:44 -0400

    Add Get-CtxOffices

---
+++ Get-CtxOffices.ps1
@@ -0,0 +1,54 @@
+﻿<#
+.SYNOPSIS
+   Returns all Citrix offices worldwide
+
+.DESCRIPTION
+   Queries web service maintained by Michael Berry
+   URL: http://autosvcdata.citrite.net/dataMgmt.svc?wsdl
+   
+.EXAMPLE
+   Get-CtxOffices
+
+.INPUTS
+   None
+
+.OUTPUTS
+   Custom PSObject with the following properties
+
+   City        : North Ryde
+   Country     : Australia
+   CountryCode : AU
+   GoogleMap   : http://goo.gl/maps/EcRJI
+   Postal      : 2113
+   Record      : 2
+   StateProv   : NSW
+   Street      : Level 3, 1 Julius Avenue Riverside Corporate Park
+
+.NOTES
+   2014-06-24   IZZETE   Initial version
+#>
+function Get-CtxOffices
+{
+    [CmdletBinding()]
+    [OutputType([PSCustomObject])]
+    Param()
+
+    try
+    {
+        $wsProxy = New-WebServiceProxy -Uri "http://autosvcdata.citrite.net/dataMgmt.svc?wsdl"
+        $result  = $wsProxy.getOffices()
+        if ($result.HasError)
+        {
+            throw $result.errormessage
+        }
+        else
+        {
+            $data = $result | ConvertFrom-Json
+            $data
+        } 
+    }
+    catch 
+    {
+        Write-Error "Exception encountered: `n" $error
+    }
+}
